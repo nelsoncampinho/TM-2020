86bbffa7c73e6eecb3ab7205d9ed8e6dc839fe24
marquesantonio2020 <marquesantonio@ipvc.pt>
2020-04-08 17:25:23 +0100

    Antonio-Animations

---
+++ TP1 Unity/TM-21674-21751/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyController : MonoBehaviour
+{
+    Animator anim;
+    public Transform player;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(player.position, this.transform.position) < 10)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                                        Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("isIdle", false);
+            if (direction.magnitude > 1.5)
+            {
+                this.transform.Translate(0, 0, 0.12f);
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAttacking", false);
+            }
+            else
+            {
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isWalking", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", false);
+        }
+    }
+}
--- TP1 Unity/TM-21674-21751/Assets/Scripts/PlayerMovement.cs
+++ TP1 Unity/TM-21674-21751/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Animator anim;
-    //public CharacterController controller;
-    //public float speed = 12f;
-    //public float gravity = -9.81f;
+    Animator anim;
+    CharacterController controller;
+
+    public float speed = 12f;
+    public float rotationSpeed = 80f;
+    public float rotation = 0f;
+    public float gravity = 8f;
     //public float jumpHeight = 3f;
+
+    public Vector3 moveDirection = Vector3.zero;
 
     //public Transform groundCheck;
     //public float groundDistance = 0.4f;
@@ -20,24 +25,21 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        GetInput();
         //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         //if (isGrounded && velocity.y < 0)
         //{
         //    velocity.y = -1f;
         //}
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        anim.SetFloat("vertical", z);
-        anim.SetFloat("horizontal", x);
 
         //Vector3 move = transform.right * x + transform.forward * z;
 
@@ -49,5 +51,71 @@ public class PlayerMovement : MonoBehaviour
         //velocity.y += gravity * Time.deltaTime;
 
         //controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Movement()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                if(anim.GetBool ("isAttacking") == true)
+                {
+                    return;
+                }
+                else if(anim.GetBool("isAttacking") == false){ 
+                    anim.SetBool("isWalking", true);
+                    anim.SetInteger("condition", 1);
+                    moveDirection = new Vector3(0, 0, 1);
+                    moveDirection *= speed * Time.deltaTime;
+                    moveDirection = transform.TransformDirection(moveDirection);
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetInteger("condition", 0);
+                moveDirection = Vector3.zero;
+            }
+        }
+        rotation += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rotation, 0);
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection);
+    }
+
+    private void GetInput()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(anim.GetBool("isWalking")== true){
+                    anim.SetBool("isWalking", false);
+                    anim.SetInteger("condition", 0);
+                }
+                if(anim.GetBool("isWalking") == false)
+                {
+                    Attacking();
+                }
+                
+            }
+        }
+    }
+
+    private void Attacking()
+    {
+        StartCoroutine(AttackRoutine());   
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        anim.SetBool("isAttacking", true);
+        anim.SetInteger("condition", 2);        
+        yield return new WaitForSeconds(1);
+        anim.SetBool("isAttacking", false);
+        anim.SetInteger("condition", 0);
+
     }
 }
